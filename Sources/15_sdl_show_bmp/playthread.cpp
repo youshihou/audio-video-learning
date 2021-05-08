06ecfd16431fa1a16a40aaa8acc216a22fb77af9
oneday <youshihou5@gmail.com>
2021-05-08 08:47:41 +0800

    update code

--- Sources/15_sdl_show_bmp/playthread.cpp
+++ Sources/15_sdl_show_bmp/playthread.cpp
@@ -2,7 +2,6 @@
 
 #include <SDL2/SDL.h>
 #include <QDebug>
-#include <QFile>
 
 
 #define END(judge, func) \
@@ -10,6 +9,7 @@ if (judge) { \
     qDebug() << #func << "error" << SDL_GetError(); \
     goto end; \
 }
+
 
 
 PlayThread::PlayThread(QObject *parent) : QThread(parent) {
@@ -30,17 +30,10 @@ void PlayThread::run() {
     SDL_Renderer *renderer = nullptr;
     SDL_Texture *texture = nullptr;
 
-//    if (SDL_Init(SDL_INIT_VIDEO)) {
-//        qDebug() << "SDL_Init error" << SDL_GetError();
-//        goto end;
-//    }
+
     END(SDL_Init(SDL_INIT_VIDEO), SDL_Init);
 
     surface = SDL_LoadBMP("/Users/ankui/Desktop/zzz/yuv/in.bmp");
-//    if (!surface) {
-//        qDebug() << "SDL_LoadBMP error" << SDL_GetError();
-//        goto end;
-//    }
     END(!surface, SDL_LoadBMP);
 
     window = SDL_CreateWindow("SDL show BMP image",
@@ -48,27 +41,24 @@ void PlayThread::run() {
                               SDL_WINDOWPOS_UNDEFINED,
                               surface->w, surface->h,
                               SDL_WINDOW_SHOWN);
-//    if (!window) {
-//        qDebug() << "SDL_CreateWindow error" << SDL_GetError();
-//        goto end;
-//    }
     END(!window, SDL_CreateWindow);
 
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) {
-        renderer = SDL_CreateRenderer(window, - 1, 0);
+        renderer = SDL_CreateRenderer(window, -1, 0);
     }
     END(!renderer, SDL_CreateRenderer);
 
     texture = SDL_CreateTextureFromSurface(renderer, surface);
     END(!texture, SDL_CreateTextureFromSurface);
 
+    END(SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE), SDL_SetRenderDrawColor);
+    END(SDL_RenderClear(renderer), SDL_RenderClear);
+
     END(SDL_RenderCopy(renderer, texture, nullptr, nullptr), SDL_RenderCopy);
 
     SDL_RenderPresent(renderer);
-
-    SDL_Delay(2000);
 
  end:
     SDL_DestroyTexture(texture);
