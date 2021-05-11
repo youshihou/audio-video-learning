2460895bbfa8c7037804eb68d434ced32d44ca08
oneday <youshihou5@gmail.com>
2021-05-11 17:16:32 +0800

    update code

--- Sources/19_sdl_play_yuv/mainwindow.cpp
+++ Sources/19_sdl_play_yuv/mainwindow.cpp
@@ -56,60 +56,6 @@ MainWindow::~MainWindow()
     SDL_Quit();
 }
 
-
-
-void sdl_show_yuv(const void * winId) {
-    SDL_Window *window = nullptr;
-    SDL_Renderer *renderer = nullptr;
-    SDL_Texture *texture = nullptr;
-
-    QFile file(FILENAME);
-
-
-    RET(SDL_Init(SDL_INIT_VIDEO), SDL_Init);
-    window = SDL_CreateWindowFrom(winId);
-    RET(!window, SDL_CreateWindow);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (!renderer) {
-        renderer = SDL_CreateRenderer(window, -1, 0);
-    }
-    RET(!renderer, SDL_CreateRenderer);
-
-    texture = SDL_CreateTexture(renderer, PIXEL_FORMAT, SDL_TEXTUREACCESS_STREAMING, IMAGE_W, IMAGE_H);
-    RET(!texture, SDL_CreateTexture);
-
-    if (!file.open(QFile::ReadOnly)) {
-        qDebug() << "file open erroe" << FILENAME;
-        goto end;
-    }
-    RET(SDL_UpdateTexture(texture, nullptr, file.readAll().constData(), IMAGE_W), SDL_UpdateTexture);
-
-    RET(SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE), SDL_SetRenderDrawColor);
-    RET(SDL_RenderClear(renderer), SDL_RenderClear);
-
-    RET(SDL_RenderCopy(renderer, texture, nullptr, nullptr), SDL_RenderCopy);
-
-    SDL_RenderPresent(renderer);
-
-    while (true) {
-        SDL_Event event;
-        SDL_WaitEvent(&event);
-        switch (event.type) {
-            case SDL_QUIT:
-            goto end;
-            break;
-        }
-    }
-
- end:
-    file.close();
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
 void MainWindow::on_playButton_clicked() {
 
     _timerId = startTimer(33);
