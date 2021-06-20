7440d6b9e76722f85a8f7def98fd1b2336827b54
oneday <youshihou5@gmail.com>
2021-06-20 23:00:55 +0800

    produce-consume-mutex-cond

---
+++ Sources/26_mutex_cond/condmutex.cpp
@@ -0,0 +1,6 @@
+#include "condmutex.h"
+
+CondMutex::CondMutex()
+{
+
+}
---
+++ Sources/26_mutex_cond/condmutex.h
@@ -0,0 +1,11 @@
+#ifndef CONDMUTEX_H
+#define CONDMUTEX_H
+
+
+class CondMutex
+{
+public:
+    CondMutex();
+};
+
+#endif // CONDMUTEX_H
